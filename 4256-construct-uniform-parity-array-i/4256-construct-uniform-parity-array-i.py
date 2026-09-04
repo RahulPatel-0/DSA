0d59d27c2f01a37ff1f7b527d12ca80f46ec60af
Rahul Patel <90706281+RahulPatel-0@users.noreply.github.com>
2026-09-04 08:05:46 +0530

    4256-CONSTRUCT-UNIFORM-PARITY-ARRAY-I Stats: Time: 0 ms (100%), Space: 19.2 MB (54.01%) - LeetHub

--- 4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.py
+++ 4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.py
@@ -1,4 +1,6 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
+        #Case 1 If all or odd/even then tru
+        #Case 2 If any element in nums is odd then we can make all are odd odd-even is odd 
         return True
         
